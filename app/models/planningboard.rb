1cf61093ae2117dd42aaec319e220bf1d63d47a8
Alex Miller <apmiller108@yahoo.com>
2015-11-05 09:26:31 -0500

    Add to search capability to search goal names as well as board names

--- app/models/planningboard.rb
+++ app/models/planningboard.rb
@@ -1,5 +1,6 @@
 class Planningboard < ActiveRecord::Base
-  has_many :comments
+  validates :name, presence: true
+  has_many :comments, dependent: :destroy
 
   mount_uploader :picture1, Picture1Uploader
   mount_uploader :picture2, Picture2Uploader
@@ -7,6 +8,9 @@ class Planningboard < ActiveRecord::Base
   mount_uploader :picture4, Picture4Uploader
   mount_uploader :picture5, Picture5Uploader
 
-  scope :search, ->(keyword) { where( "lower(name) LIKE ?", "%#{keyword.downcase}%") if keyword.present? }
+  scope :search, ->(keyword) { where( "lower(name) LIKE ? OR lower(goal1) LIKE ? OR lower(goal2) LIKE ?
+    OR lower(goal3) LIKE ? OR lower(goal4) LIKE ? OR lower(goal5) LIKE ?",
+    "%#{keyword.downcase}%", "%#{keyword.downcase}%", "%#{keyword.downcase}%", "%#{keyword.downcase}%",
+    "%#{keyword.downcase}%", "%#{keyword.downcase}%" ) if keyword.present? }
 
 end
